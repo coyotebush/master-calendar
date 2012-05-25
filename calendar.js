1fc5e81d93af939f27ea31cd68e7123aa0dc0d72
Corey Ford <coyotebush22@gmail.com>
2012-05-25 00:19:18 -0700

    working on hashchange handling

--- calendar.js
+++ calendar.js
@@ -30,8 +30,11 @@ $(function(){
 			$("#create_menu").show('fast').css('top', jsEvent.pageY).css('left', jsEvent.pageX);
 		},
 		viewDisplay: function(view) {
-			if (cal.data('loaded'))
+			if (cal.data('loaded')) {
+				console.log('viewDisplay: ' + view.start.getTime());
+				cal.data('savedView', view);
 				$.bbq.pushState({view: view.name, date: view.start.getTime()});
+			}
 			else
 				cal.data('loaded', 1);
 		}
@@ -66,5 +69,33 @@ $(function(){
 		$('#calendar').fullCalendar('refetchEvents');
 		return false;
 	});
+	$(window).on('hashchange', function() {
+		console.log('--- hashchange');
+		var oldView = cal.data('savedView');
+		var date = $.bbq.getState('date');
+
+		console.log(date);
+		console.log(oldView);
+		if (oldView)
+			console.log(oldView.start.getTime());
+
+		if (date) {
+			if (!(oldView && date == oldView.start.getTime())) {
+				// this state differs from that saved in the viewDisplay
+				// handler, so we're not being triggered from there.
+				console.log('set');
+				cal.fullCalendar('gotoDate', new Date(date + 15 * 24 * 60 * 60 * 1000));
+			}
+			else
+				console.log('nada');
+		}
+		else {
+			// the hash was probably cleared, almost certainly due to a
+			// browser action rather than calendar navigation controls
+			console.log('reset');
+			cal.fullCalendar('today');
+		}
+		//cal.data('savedView', date);
+	});//.trigger('hashchange');
 });
 
