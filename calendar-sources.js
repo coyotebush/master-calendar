0dfd26bc0881e1003a43bd47819a6cb3763e4b24
Corey Ford <coyotebush22@gmail.com>
2012-06-30 09:52:24 -0700

    switch to Cal Poly CSC as test gcal

--- calendar-sources.js
+++ calendar-sources.js
@@ -13,8 +13,8 @@ var myEventSources = [
 	color: '#006600',
 },
 {
-	name: 'SLO Swing',
-	url: 'https://www.google.com/calendar/feeds/gflsystems.com_kav7dpb8rmcqf6eevbibibfpes%40group.calendar.google.com/public/basic',
+	name: 'Cal Poly CSC',
+	url: 'https://www.google.com/calendar/feeds/u2fkgbv5g0v021r31uh9u9jhig%40group.calendar.google.com/public/basic',
 	color: '#550055',
 	currentTimezone: 'America/Los_Angeles'
 }
