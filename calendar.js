8f9ed6a6d187cb008f82db9fd7e041addac77ea6
Corey Ford <coyotebush22@gmail.com>
2012-06-30 21:18:25 -0700

    clean up code formatting

--- calendar.js
+++ calendar.js
@@ -5,16 +5,20 @@ $(function(){
 
 	var startHash = location.hash.slice(1).split('/');
 	var startView = startHash[0] || defaultView;
-	var startDate = startHash[1] && $.fullCalendar.parseISO8601(startHash[1], true)
+	var startDate =
+		startHash[1] && $.fullCalendar.parseISO8601(startHash[1], true)
 		|| new Date();
 
 	// Hash format: [view[/date]]
 	var setHash = function(viewObj) {
 		var hash = '#';
-		if (Date.now() < viewObj.start || Date.now() > viewObj.end)
-			hash += viewObj.name + '/' + $.fullCalendar.formatDate(viewObj.start, 'yyyy-MM-dd');
-		else if (viewObj.name != defaultView)
+		if (Date.now() < viewObj.start || Date.now() > viewObj.end) {
+			hash += viewObj.name + '/' +
+				$.fullCalendar.formatDate(viewObj.start, 'yyyy-MM-dd');
+		}
+		else if (viewObj.name != defaultView) {
 			hash += viewObj.name;
+		}
 		location.replace(hash);
 	};
 
@@ -56,24 +60,29 @@ $(function(){
 		},
 		eventRender: function(event, element) {
 			if (event.body) {
-				$.jqml(event.body).appendTo($('<div/>').appendTo($(element).children().first()));
+				$(element).children().first()
+					.append($('<div/>')
+						.append($.jqml(event.body)));
 			}
 			if (event.participation) {
-				$(element).find('.fc-event-time').addClass('highlight-text');
+				$(element).find('.fc-event-time')
+					.addClass('highlight-text');
 			}
 		},
 		select: function(startDate, endDate, allDay, jsEvent, view) {
-			$('#create-menu a').attr('href', function() {
-				var api = $(this).data('api');
-				var url = api.url || api;
-				var params = {};
-				params[api.startParam || 'start'] = startDate.getTime();
-				params[api.endParam   || 'end'  ] = endDate.getTime();
-				url += url.indexOf('?') > -1 ? '&' : '?';
-				url += $.param(params);
-				return url;
-			});
-			$('#create-popup').show()
+			$('#create-menu a')
+				.attr('href', function() {
+					var api = $(this).data('api');
+					var url = api.url || api;
+					var params = {};
+					params[api.startParam || 'start'] = startDate.getTime();
+					params[api.endParam   || 'end'  ] = endDate.getTime();
+					url += url.indexOf('?') > -1 ? '&' : '?';
+					url += $.param(params);
+					return url;
+				});
+			$('#create-popup')
+				.show()
 				.position({
 					of: jsEvent,
 					my: 'left top',
@@ -91,56 +100,64 @@ $(function(){
 
 	$(myEventSources).each(function(index) {
 		if (this.api) {
-			if (typeof this.api.events == 'object')
-				$.extend(this, this.api.events);
-			else
-				this.url = this.api.events;
+			if (this.api.events) {
+				if (typeof this.api.events == 'object') {
+					$.extend(this, this.api.events);
+				}
+				else {
+					this.url = this.api.events;
+				}
+			}
 
-			if (this.api.create)
-				$('#create-menu').append(
-					$('<li/>').append(
-						$('<a>' + this.name + '</a>')
+			if (this.api.create) {
+				$('#create-menu')
+					.append($('<li/>')
+						.append($('<a>' + this.name + '</a>')
 							.data('api', this.api.create)
 							.click(function() {
 								if (this.href) {
 									window.open(this.href);
 									return false;
 								}
-							})
-					)
-				);
+							})));
+			}
 		}
 
-		$('#sources').append(
-			$('<li class="ui-widget"/>')
+		$('#sources')
+			.append($('<li class="ui-widget"/>')
 				.css('background-color', this.color)
 				.css('color', '#ffffff')
-				.append(
-					$('<label for="source' + index + '">' + this.name + '</label>')
-						.prepend(
-							$('<input type="checkbox" checked="checked" id="source' + index + '">')
-								.data('source', this)
-								.change(function() {
-									cal.fullCalendar($(this).is(':checked')
-										? 'addEventSource' : 'removeEventSource',
-										$(this).data('source'));
-								})
-								.prop('checked', this.defaultEnable !== false)
-								.change()
-						)
-					)
-		);
+				.append($('<label for="source' + index + '">' + this.name + '</label>')
+					.prepend($('<input type="checkbox" id="source' + index + '">')
+						.data('source', this)
+						.change(function() {
+							cal.fullCalendar(
+								$(this).is(':checked') ? 'addEventSource' : 'removeEventSource',
+								$(this).data('source'));
+						})
+						.prop('checked', this.defaultEnable !== false)
+						.change())));
 	});
-	$('#create-popup-close').button({ text: false, icons: { primary: 'ui-icon-closethick' } })
-		.click(function() { cal.fullCalendar('unselect'); });
-	$('#refresh').button().click(function() {
-		cal.fullCalendar('refetchEvents');
-	});
-	$(document).keydown(function(e) {
-		if (e.which == 27)
+
+	$('#create-popup-close')
+		.button({ text: false, icons: { primary: 'ui-icon-closethick' } })
+		.click(function() {
 			cal.fullCalendar('unselect');
+		});
+	$(document).keydown(function(e) {
+		if (e.which == 27) {
+			cal.fullCalendar('unselect');
+		}
 	});
+	$('#refresh')
+		.button()
+		.click(function() {
+			cal.fullCalendar('refetchEvents');
+		});
+
 	$(window).resize(resizeCalendar);
 	resizeCalendar();
 });
+
+/* vim: set sw=2 ts=2 noet */
 
