315f69fc72dce09dbf87625bee3bfe075a6dd53e
Corey Ford <coyotebush22@gmail.com>
2012-07-31 17:52:34 -0700

    throw init errors asynchronously

--- MasterCalendar.js
+++ MasterCalendar.js
@@ -5,16 +5,16 @@ var MasterCalendar = {
 	modules: {},
 	init: function () {
 		'use strict';
-		var cal = $('#calendar'), options = {}, m;
+		var cal = $('#calendar'), options = {}, m, throwIt = function (e) {
+			return function () { throw e; };
+		};
 		for (m in MasterCalendar.modules) {
 			if (MasterCalendar.modules.hasOwnProperty(m)
 					&& $.isFunction(MasterCalendar.modules[m])) {
 				try {
 					$.extend(options, MasterCalendar.modules[m](cal, MasterCalendar.sources));
 				} catch (e) {
-					if (window.console) {
-						console.error(e);
-					}
+					setTimeout(throwIt(e), 0);
 				}
 			}
 		}
