1cc3d76138b7ce201e972ef38034fae71099bd2b
Corey Ford <coyotebush22@gmail.com>
2012-08-02 20:30:30 -0700

    apply YUI compressor

--- build.sh
+++ build.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 BUILD=build
-rm -rf build/*
-cp index.html calendar-sources.js $BUILD
-cat `sed -nE "s/@import \"(.*)\";/\1/p" calendar.css` > $BUILD/calendar.css
-cat `sed -nE "s/js\('(.*)'\);/\1/p" calendar.js` > $BUILD/calendar.js
-cp -R jquery/smoothness/images $BUILD
+rm -rf $BUILD && mkdir $BUILD
+cp -R index.html calendar-sources.js jquery/smoothness/images $BUILD
+cat `sed -nE "s/@import \"(.*)\";/\1/p" calendar.css` \
+	| yui-compressor --type css -o $BUILD/calendar.css
+cat `sed -nE "s/js\('(.*)'\);/\1/p" calendar.js` \
+	| yui-compressor --type js -o $BUILD/calendar.js
