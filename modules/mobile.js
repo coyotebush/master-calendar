cc2db9ef93cc9b7b92c97fb431b8f60362722fa2
Corey Ford <coyotebush22@gmail.com>
2012-08-21 13:11:40 -0700

    hide title in list view on small screens

--- modules/mobile.js
+++ modules/mobile.js
@@ -4,6 +4,12 @@
 /* vim: set sw=2 ts=2 noet */
 MasterCalendar.modules.mobile = function (cal) {
 	'use strict';
+	cal.on('viewDisplay', function () {
+		cal.find('.fc-header-title')
+			.toggleClass('fc-header-title-hidden',
+				(cal.fullCalendar('getView').name === 'list' && cal.css('position') === 'static')
+			);
+	});
 	if (cal.css('position') === 'static') {
 		return { defaultView: 'list' };
 	}
