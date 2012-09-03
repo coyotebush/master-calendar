da3a817398eb9b7e07b4bb6f0b7232250a515b3a
Corey Ford <coyotebush22@gmail.com>
2012-09-02 17:46:58 -0700

    create RefreshView

--- modules/sources.js
+++ modules/sources.js
@@ -2,8 +2,35 @@
 /*jslint browser: true, eqeq: true */
 /*global $: false, MasterCalendar: false */
 /* vim: set sw=2 ts=2 noet */
+'use strict';
+var RefreshView = function (options) {
+	this.el = options.el;
+	this.cal = options.cal;
+	this.el.find('.ui-button')
+		.button({ text: false, icons: { primary: 'ui-icon-refresh' } })
+		.removeClass('ui-corner-all').addClass('ui-corner-left')
+		.click($.proxy(this.click, this));
+
+	this.el.find('.ui-progressbar')
+		.progressbar({ value: 100 })
+		.removeClass('ui-corner-all').addClass('ui-corner-right')
+		.children('.ui-progressbar-value').removeClass('ui-corner-left');
+};
+
+RefreshView.prototype.click = function () {
+	this.cal.fullCalendar('refetchEvents');
+};
+
+RefreshView.prototype.update = function (pending, total) {
+	var percentComplete = 100 * (1 - pending / (total - 1)), that = this;
+	this.el.find('.ui-progressbar-value')
+		.stop(true, true)
+		.animate({ width: percentComplete + '%' }, function () {
+			that.el.find('.ui-progressbar').progressbar('value', percentComplete);
+		});
+};
+
 MasterCalendar.modules.sources = function (cal, sources) {
-	'use strict';
 	$(sources).each(function () {
 		// {{{ Checklist
 		$('#sources')
@@ -30,27 +57,10 @@ MasterCalendar.modules.sources = function (cal, sources) {
 	});
 	// }}}
 
-	$('#refresh .ui-button')
-		.button({ text: false, icons: { primary: 'ui-icon-refresh' } })
-		.removeClass('ui-corner-all').addClass('ui-corner-left')
-		.click(function () {
-			cal.fullCalendar('refetchEvents');
-		});
-
-	$('#refresh .ui-progressbar')
-		.progressbar({ value: 100 })
-		.removeClass('ui-corner-all').addClass('ui-corner-right')
-		.children('.ui-progressbar-value').removeClass('ui-corner-left');
+	var refresh = new RefreshView({ cal: cal, el: $('#refresh') });
 
 	return {
-		loading: function (pending, total) {
-			var percentComplete = 100 * (1 - pending / (total - 1));
-			$('#refresh .ui-progressbar-value')
-				.stop(true, true)
-				.animate({ width: percentComplete + '%' }, function () {
-					$('#refresh > div').progressbar('value', percentComplete);
-				});
-		}
+		loading: $.proxy(refresh.update, refresh)
 	};
 };
 
