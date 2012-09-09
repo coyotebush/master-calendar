e31127d6ad2ad3fa2dda9a0106b9c754df182750
Corey Ford <coyotebush22@gmail.com>
2012-09-08 20:09:25 -0700

    create CalendarMobile

--- modules/mobile.js
+++ modules/mobile.js
@@ -2,15 +2,25 @@
 /*jslint browser: true */
 /*global $: false, MasterCalendar: false */
 /* vim: set sw=2 ts=2 noet */
+'use strict';
+
+var CalendarMobile = function (options) {
+	this.cal = options.cal;
+};
+
+CalendarMobile.prototype.isActive = function () {
+	return this.cal.css('position') === 'static';
+};
+
+CalendarMobile.prototype.update = function (e, viewObj) {
+	this.cal.find('.fc-header-title')
+		.toggleClass('fc-header-title-hidden', viewObj.name === 'list');
+};
+
 MasterCalendar.modules.mobile = function (cal) {
-	'use strict';
-	cal.on('viewDisplay', function () {
-		cal.find('.fc-header-title')
-			.toggleClass('fc-header-title-hidden',
-				(cal.fullCalendar('getView').name === 'list' && cal.css('position') === 'static')
-			);
-	});
-	if (cal.css('position') === 'static') {
+	var mobile = new CalendarMobile({ cal: cal });
+	cal.on('viewDisplay', $.proxy(mobile.update, mobile));
+	if (mobile.isActive()) {
 		return { defaultView: 'list' };
 	}
 };
